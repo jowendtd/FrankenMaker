7d8a83a4c2fe9f227a2bffb66019a573dc289757
John Owen <jowendtd@gmail.com>
2023-06-03 22:41:42 -0500

    reduce y size to 180 for clips

--- firmware/Marlin/Configuration.h
+++ firmware/Marlin/Configuration.h
@@ -1766,7 +1766,7 @@
 
 // The size of the printable area
 #define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define Y_BED_SIZE 180
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
